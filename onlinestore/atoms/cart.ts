2e1c59d1d6e34479107d396812813434c899c79c
Aadarsh Ghimire <aadarshghimire524@gmail.com>
2023-09-03 12:06:31 +0545

    deploy

--- onlinestore/atoms/cart.ts
+++ onlinestore/atoms/cart.ts
@@ -2,7 +2,7 @@
 import { atom } from 'recoil';
 import { Product } from "@/types";
 
-
+//cartState 
 export const cartState = atom({
   key: 'cartState',
   default: [] as Product[], // Initialize the cart as an empty array
